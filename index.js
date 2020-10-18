34311fb2d4362db42bb6ae75a4b5a644634ca3b1
Emily M <30880308+e0marina@users.noreply.github.com>
2020-10-17 18:07:10 -0700

    updated some credentials

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-require('dotenv-safe').load();
-const http = require('http');
-const express = require('express');
-const {urlencoded} = require('body-parser');
-const twilio = require('twilio');
+require("dotenv-safe").load();
+const http = require("http");
+const express = require("express");
+const { urlencoded } = require("body-parser");
+const twilio = require("twilio");
 const ClientCapability = twilio.jwt.ClientCapability;
 const VoiceResponse = twilio.twiml.VoiceResponse;
 
 let app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(urlencoded({extended: false}));
+app.use(express.static(__dirname + "/public"));
+app.use(urlencoded({ extended: false }));
 
 // Generate a Twilio Client capability token
-app.get('/token', (request, response) => {
+app.get("/token", (request, response) => {
   const capability = new ClientCapability({
     accountSid: process.env.TWILIO_ACCOUNT_SID,
     authToken: process.env.TWILIO_AUTH_TOKEN,
@@ -21,7 +21,8 @@ app.get('/token', (request, response) => {
 
   capability.addScope(
     new ClientCapability.OutgoingClientScope({
-      applicationSid: process.env.TWILIO_TWIML_APP_SID})
+      applicationSid: process.env.TWILIO_TWIML_APP_SID,
+    })
   );
 
   const token = capability.toJwt();
@@ -33,12 +34,15 @@ app.get('/token', (request, response) => {
 });
 
 // Create TwiML for outbound calls
-app.post('/voice', (request, response) => {
+app.post("/voice", (request, response) => {
   let voiceResponse = new VoiceResponse();
-  voiceResponse.dial({
-    callerId: process.env.TWILIO_NUMBER,
-  }, request.body.number);
-  response.type('text/xml');
+  voiceResponse.dial(
+    {
+      callerId: process.env.TWILIO_NUMBER,
+    },
+    request.body.number
+  );
+  response.type("text/xml");
   response.send(voiceResponse.toString());
 });
 
